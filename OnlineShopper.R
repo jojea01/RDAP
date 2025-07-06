afc647e5c9f4b9ccba02709da76208ba645895ba
ColeMcNulty <70773977+ColeMcNulty@users.noreply.github.com>
2025-07-05 20:53:39 -0400

    Update OnlineShopper.R

--- OnlineShopper.R
+++ OnlineShopper.R
@@ -15,4 +15,56 @@ cov(shopper_num)
 shopper_target <- data.frame(shopper[,18])
 summary(shopper_target)
 
-Test 
+
+#--Cleaning 
+
+#Dropping Nas
+shopper_clean <- shopper[complete.cases(shopper), ]
+sum(is.na(shopper_clean))  
+
+#--- Changing nominal variables to numerical-------------
+
+shopper_clean$month_num <- match(shopper_clean$Month, month.abb)
+shopper_clean <- subset(shopper_clean, select = -Month)
+
+shopper_clean$Revenue <- ifelse(shopper_clean$Revenue == "TRUE", 1,
+                     ifelse(shopper_clean$Revenue == "FALSE", 2,NA))
+
+shopper_clean$VisitorType <- ifelse(shopper_clean$VisitorType == "Returning_Visitor", 1,
+                                ifelse(shopper_clean$VisitorType == "New_Visitor", 2,NA))
+
+
+shopper_clean$Weekend <- ifelse(shopper_clean$Weekend == "TRUE", 1,
+                                ifelse(shopper_clean$Weekend == "FALSE", 2,NA))
+
+shopper_clean$Revenue <- as.numeric(shopper_clean$Revenue)
+shopper_clean$Weekend <- as.numeric(shopper_clean$Weekend)
+shopper_clean$VisitorType <- as.numeric(shopper_clean$VisitorType)
+
+
+install.packages("corrplot")
+library(corrplot)
+
+cor <-cor(shopper_clean)
+print(cor)
+corrplot(cor, method = "color", addCoef.col = "black")
+
+plot(cor)
+
+
+#-- Droping variables Exit Rate ProductRelated_Duration Administrative_Duration Infromational_Duration  ----------
+
+shop_drop <- shopper_clean[, !(names(shopper_clean) %in% c("ProductRelated_Duration", "Administrative_Duration", "Informational_Duration"))]
+shop_drop
+
+
+#---SVM---- Clean
+
+drop 
+library(e1071) 
+library(rpart) 
+set.seed(623) 
+TUNE.kyposis <- tune(svm, Revenue ~., data=shop_drop,  
+                     kernel="linear",  
+                     scale=FALSE,  
+                     ranges=list(cost=c(0.01, 1, 10))) 
