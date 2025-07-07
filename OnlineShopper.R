afd1698f28bc0885a247402297be44fd34e6747a
jojea01 <140738759+jojea01@users.noreply.github.com>
2025-07-07 12:34:27 -0400

    Update OnlineShopper.R

--- OnlineShopper.R
+++ OnlineShopper.R
@@ -14,33 +14,6 @@ cov(shopper_num)
 shopper_target <- data.frame(shopper[,18])
 summary(shopper_target)
 
-#----- bar graph------
-shopper_rev <- sum(shopper_clean$Revenue == 1, na.rm = TRUE)
-shopper_rev
-shopper_norev <- sum(shopper_clean$Revenue == 2, na.rm = TRUE)
-shopper_norev
-
-# --- Create matrix for barplot ---
-counts_matrix <- matrix(c(shopper_rev, shopper_norev), 
-                        nrow = 2, 
-                        byrow = TRUE)
-
-rownames(counts_matrix) <- c("Revenue", "No Revenue")
-colnames(counts_matrix) <- c("Revenue Status")
-
-barplot(counts_matrix,
-        main = "Shopper Revenue Status",
-        ylab = "Count",
-        col = c("blue", "red"),
-        beside = TRUE)
-legend("topleft",
-       legend = c("Revenue", "No Revenue"),
-       fill = c("blue", "red"),
-       xpd = TRUE,
-       bty = "n")
-
-print(counts_matrix)
-
 #--- Changing nominal variables to numerical-------------
 shopper_clean <- shopper
 
@@ -61,7 +34,36 @@ shopper_clean$Revenue <- as.numeric(shopper_clean$Revenue)
 shopper_clean$Weekend <- as.numeric(shopper_clean$Weekend)
 shopper_clean$VisitorType <- as.numeric(shopper_clean$VisitorType)
 
+# summary statistics
+summary(shopper_clean)
 
+#----- bar graph target------
+shopper_rev <- sum(shopper_clean$Revenue == 1, na.rm = TRUE)
+shopper_rev
+shopper_norev <- sum(shopper_clean$Revenue == 2, na.rm = TRUE)
+shopper_norev
+
+counts_matrix <- matrix(c(shopper_rev, shopper_norev), 
+                        nrow = 2, 
+                        byrow = TRUE)
+
+rownames(counts_matrix) <- c("Revenue", "No Revenue")
+colnames(counts_matrix) <- c("Revenue Status")
+
+barplot(counts_matrix,
+        main = "Shopper Revenue Status",
+        ylab = "Count",
+        col = c("blue", "red"),
+        beside = TRUE)
+legend("topleft",
+       legend = c("Revenue", "No Revenue"),
+       fill = c("blue", "red"),
+       xpd = TRUE,
+       bty = "n")
+
+print(counts_matrix)
+
+# correlation plot
 install.packages("corrplot")
 library(corrplot)
 
@@ -85,7 +87,7 @@ shop_fit <- lm(Revenue ~ ., data=shopper_clean)
 summary(shop_fit)
 anova(shop_fit)
 
-#-- Droping variables from anova ----------
+#-- Dropping variables from anova ----------
 
 shop_drop <- shopper_clean[, !(names(shopper_clean) %in% c("Browser", "Region", "TrafficType", "Weekend"))]
 
@@ -119,11 +121,11 @@ TUNE.kyposis <- tune(svm, Revenue ~., data=shop_drop[T_set,],
                      scale=FALSE,  
                      ranges=list(cost=c(0.01, 1, 10))) 
 
-#-- Partitioning
+#-- Partitioning2.0
 set.seed(1115)
 
 df_n_30 <- nrow(shop_drop)
-n_V_set_30 <- floor(df_n_30 * 0.3)
+n_V_set_30 <- floor(df_n_30 * 0.30)
 
 # Sample row indices for validation set
 val_indices_30 <- sample(x = df_n_30, size = n_V_set_30, replace = FALSE)
@@ -143,6 +145,10 @@ whole_prediction_devi <- predict(tree_devi, newdata = T_set_30, type="vector")
 pred_tree_shop_30 <- predict(tree_devi, newdata=V_set_30, type="vector")
 no_na_tree_30 <- na.omit(round(pred_tree_shop_30)- V_set_30$Revenue)
 mse_30_tree <- mean((no_na_tree_30)^2)
+
+# confusion matrix
+conf_mat_tree <- table(Predicted = round(pred_tree_shop_30), Actual = V_set_30$Revenue)
+print(conf_mat_tree)
 
 # tree plot
 plot(tree_devi)
@@ -189,37 +195,43 @@ mse_30_tree
 library(randomForest)
 
 set.seed(1115)
-BG_shop <- randomForest(Revenue ~ ., data=T_set_30, ntree=500, importance=TRUE, na.action=na.omit)
+RF_shop <- randomForest(Revenue ~ ., data=T_set_30, ntree=500, importance=TRUE, na.action=na.omit)
+RF_shop
 
 # Cross-Validation MSE
-pred_BG_shop_30_RF <- predict(BG_shop, newdata=V_set_30)
-no_na_BG_30_RF <- na.omit(round(pred_BG_shop_30_RF) - V_set_30$Revenue)
-mse_30_RF <- mean((no_na_BG_30_RF)^2)
+pred_shop_30_RF <- predict(RF_shop, newdata=V_set_30)
+no_na_30_RF <- na.omit(round(pred_shop_30_RF) - V_set_30$Revenue)
+mse_30_RF <- mean((no_na_30_RF)^2)
 
-plot(pred_BG_shop_30_RF, V_set_30$Revenue, main="Bagging, using a validation set", xlab="y-predicted", ylab="y-observed")
+# confusion matrix
+conf_mat_RF <- table(Predicted = round(pred_shop_30_RF), Actual = V_set_30$Revenue)
+print(conf_mat_RF)
+
+plot(pred_shop_30_RF, V_set_30$Revenue, main="Random Forest, using a validation set", xlab="y-predicted", ylab="y-observed")
   
-varImpPlot(BG_shop, sort=TRUE, scale=TRUE)
+varImpPlot(RF_shop, sort=TRUE, scale=TRUE)
 
 # sorted importance
-apply(importance(BG_shop, type=1, scale=TRUE), 2, sort, decreasing=TRUE)
+apply(importance(RF_shop, type=1, scale=TRUE), 2, sort, decreasing=TRUE)
 
-apply(importance(BG_shop, type=2, scale=TRUE), 2, sort, decreasing=TRUE)
+apply(importance(RF_shop, type=2, scale=TRUE), 2, sort, decreasing=TRUE)
+
+# Plot random forest error rates across trees
+plot(RF_shop)
 
 # stability test
 
-pred_BG_shop_20_RF <- predict(BG_shop, newdata=V_set_20)
-no_na_BG_20_RF <- na.omit(round(pred_BG_shop_20_RF) - V_set_20$Revenue)
+pred_shop_20_RF <- predict(RF_shop, newdata=V_set_20)
+no_na_20_RF <- na.omit(round(pred_BG_shop_20_RF) - V_set_20$Revenue)
 mse_20_RF <- mean((no_na_BG_20_RF)^2)
 
-pred_BG_shop_10_RF <- predict(BG_shop, newdata=V_set_10)
-no_na_BG_10_RF <- na.omit(round(pred_BG_shop_10_RF) - V_set_10$Revenue)
-mse_10_RF <- mean((no_na_BG_10_RF)^2)
+pred_shop_10_RF <- predict(RF_shop, newdata=V_set_10)
+no_na_10_RF <- na.omit(round(pred_shop_10_RF) - V_set_10$Revenue)
+mse_10_RF <- mean((no_na_10_RF)^2)
 
 mse_10_RF
 mse_20_RF
 mse_30_RF
-
-
 
 #----K-Means-----
 
