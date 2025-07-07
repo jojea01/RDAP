44bebd67517296344d4c447ece02273c89e5c4c9
jojea01 <140738759+jojea01@users.noreply.github.com>
2025-07-06 22:51:02 -0400

    Update OnlineShopper.R
    tree and rf

--- OnlineShopper.R
+++ OnlineShopper.R
@@ -92,40 +92,105 @@ TUNE.kyposis <- tune(svm, Revenue ~., data=shop_drop[T_set,],
                      scale=FALSE,  
                      ranges=list(cost=c(0.01, 1, 10))) 
 
-#---- tree -----
-tree_Oz_devi <- tree(V4 ~ ., split="deviance", data=Ozone.TRN, na.action=na.omit)
-head(tree_Oz_devi$frame)
+#-- Partitioning
+set.seed(1115)
 
-whole_prediction_devi <- predict(tree_Oz_devi, newdata = Ozone.VLD, type="vector")
+df_n_30 <- nrow(shop_drop)
+n_V_set_30 <- floor(df_n_30 * 0.3)
+
+# Sample row indices for validation set
+val_indices_30 <- sample(x = df_n_30, size = n_V_set_30, replace = FALSE)
+
+V_set_30 <- shop_drop[val_indices_30, ]
+T_set_30 <- shop_drop[-val_indices_30, ]
+
+#---- tree -----
+library(tree)
+
+tree_devi <- tree(Revenue ~ ., split="deviance", data=T_set_30, na.action=na.omit)
+head(tree_devi$frame)
+
+whole_prediction_devi <- predict(tree_devi, newdata = T_set_30, type="vector")
 
 # Cross-validation
-pred_tree_Oz <- predict(tree_Oz_devi, newdata=Ozone.VLD, type="vector")
-no_na_tree <- na.omit(round(pred_tree_Oz)- Ozone.VLD$V4)
-mean((no_na_tree)^2)
+pred_tree_shop_30 <- predict(tree_devi, newdata=V_set_30, type="vector")
+no_na_tree_30 <- na.omit(round(pred_tree_shop)- V_set_30$Revenue)
+mse_30_tree <- mean((no_na_tree_30)^2)
 
 # tree plot
-plot(tree_Oz_devi)
-title(main="Classification Tree by deviance")
-text(tree_Oz_devi, cex=0.75, pretty=0)
+plot(tree_devi)
+title(main="Classification Tree of Shopper by deviance")
+text(tree_devi, cex=0.75, pretty=0)
+
+# stability test
+set.seed(1115)
+
+df_n_20 <- nrow(shop_drop)
+n_V_set_20 <- floor(df_n_20 * 0.20)
+n_T_set_20 <- df_n_20 - n_V_set_20
+
+val_indices_20 <- sample(x = df_n_20, size = n_V_set_20, replace = FALSE)
+
+V_set_20 <- shop_drop[val_indices_20, ]
+T_set_20 <- shop_drop[-val_indices_20, ]
+
+pred_tree_shop_20 <- predict(tree_devi, newdata=V_set_20, type="vector")
+no_na_tree_20 <- na.omit(round(pred_tree_shop_20)- V_set_20$Revenue)
+mse_20_tree <- mean((no_na_tree_20)^2)
+
+set.seed(1115)
+
+df_n_10 <- nrow(shop_drop)
+n_V_set_10 <- floor(df_n_10 * 0.3)
+
+# Sample row indices for validation set
+val_indices_10 <- sample(x = df_n_10, size = n_V_set_10, replace = FALSE)
+
+V_set_10 <- shop_drop[val_indices_10, ]
+T_set_10 <- shop_drop[-val_indices_10, ]
+
+pred_tree_shop_10 <- predict(tree_devi, newdata=V_set_10, type="vector")
+no_na_tree_10 <- na.omit(round(pred_tree_shop_10)- V_set_10$Revenue)
+mse_10_tree <- mean((no_na_tree_10)^2)
+
+mse_10_tree
+mse_20_tree
+mse_30_tree
+
 
 #--- random forest
+library(randomForest)
 
-set.seed(441)
-BG_Oz <- randomForest(V4 ~ ., data=Ozone.TRN, ntree=500, importance=TRUE, na.action=na.omit)
+set.seed(1115)
+BG_shop <- randomForest(Revenue ~ ., data=T_set, ntree=500, importance=TRUE, na.action=na.omit)
+
 # Cross-Validation MSE
-pred_BG_Oz <- predict(BG_Oz, newdata=Ozone.VLD)
-no_na_BG <- na.omit(round(pred_BG_Oz)- Ozone.VLD$V4)
-mean((no_na_BG)^2)
+pred_BG_shop_30_RF <- predict(BG_shop, newdata=V_set_30)
+no_na_BG_30_RF <- na.omit(round(pred_BG_shop_30_RF) - V_set_30$Revenue)
+mse_30_RF <- mean((no_na_BG_30_RF)^2)
 
-plot(pred_BG_Oz, Ozone.VLD$V4, main="Bagging, using a test set", xlab="y-predicted", ylab="y-observed")
-abline(a=0, b=1, col="blue")
-
-varImpPlot(BG_Oz, sort=TRUE, scale=TRUE) ## Default: sort=TRUE & scale=TRUE
+plot(pred_BG_shop, V_set$Revenue, main="Bagging, using a validation set", xlab="y-predicted", ylab="y-observed")
+  
+varImpPlot(BG_shop, sort=TRUE, scale=TRUE)
 
 # sorted importance
-apply(importance(BG_Oz, type=1, scale=TRUE), 2, sort, decreasing=TRUE)
+apply(importance(BG_shop, type=1, scale=TRUE), 2, sort, decreasing=TRUE)
 
-apply(importance(BG_Oz, type=2, scale=TRUE), 2, sort, decreasing=TRUE)
+apply(importance(BG_shop, type=2, scale=TRUE), 2, sort, decreasing=TRUE)
+
+# stability test
+
+pred_BG_shop_20_RF <- predict(BG_shop, newdata=V_set_20)
+no_na_BG_20_RF <- na.omit(round(pred_BG_shop_20_RF) - V_set_20$Revenue)
+mse_20_RF <- mean((no_na_BG_20_RF)^2)
+
+pred_BG_shop_10_RF <- predict(BG_shop, newdata=V_set_10)
+no_na_BG_10_RF <- na.omit(round(pred_BG_shop_10_RF) - V_set_10$Revenue)
+mse_10_RF <- mean((no_na_BG_10_RF)^2)
+
+mse_10_RF
+mse_20_RF
+mse_30_RF
 
 
 #----K-Means-----
